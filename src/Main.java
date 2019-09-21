cf4a42d85edc1a6ad1df1dd38ef36c0f5a302f9b
Roman <groma_92@mail.ru>
2019-09-21 03:21:02 +0300

    delete comments

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: За каждую полную тысячу рублей: 50 бонусов
-        int amountForPurchases = 9_000; // сумма предыдущих покупок
-        int purchases = 5_000; // стоимость текущей покупки
+        int amountForPurchases = 9_000;
+        int purchases = 5_000;
         int amountForBonus = 1000;
         int bonusesPerAmount = 50;
         int bonus = purchases / amountForBonus * bonusesPerAmount;
-        int accumulatedBonuses = amountForPurchases / amountForBonus * bonusesPerAmount; // вычисление бонусов за предыдущие покупки
+        int accumulatedBonuses = amountForPurchases / amountForBonus * bonusesPerAmount;
 
         System.out.println(accumulatedBonuses);
         System.out.println(bonus);
