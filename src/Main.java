c9d9c24986f8b37aa9a490d18b9e56acb42b9bee
Roman <groma_92@mail.ru>
2019-09-25 00:44:43 +0300

    delete comments

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: За каждую полную тысячу рублей: 50 бонусов
-        int amountForPurchases = 9_000;
-        int purchases = 5_000;
+        int amountForPurchases = 20_000;
+        int purchases = 20_000;
+        byte blueCard = 1;
+        byte silverCard = 1;
+        byte goldCard = 1;
         int amountForBonus = 1000;
-        int bonusesPerAmount = 50;
+        byte bonusesPerAmount = 50;
         int bonus = purchases / amountForBonus * bonusesPerAmount;
         int accumulatedBonuses = amountForPurchases / amountForBonus * bonusesPerAmount;
 
-        System.out.println(accumulatedBonuses);
-        System.out.println(bonus);
+
+        if (blueCard >=1 && blueCard <= 15_000) {
+            System.out.println(accumulatedBonuses);
+            System.out.println(bonus);
+        } else {
+            if (silverCard >= 15_001 && silverCard <= 150_000)
+            System.out.println(accumulatedBonuses);
+            System.out.println(bonus);
+        } if (goldCard >= 150_000){
+            System.out.println(accumulatedBonuses);
+            System.out.println(bonus);
+        }
+
+        }
     }
-}
+
