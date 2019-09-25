de6625c3fd45d5bfb52eff6649f7b9b573c15aea
Roman <groma_92@mail.ru>
2019-09-25 16:48:06 +0300

    исправил ошибку

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,24 @@ public class Main {
         int amountForPurchases = 20_000;
         int purchases = 200_000;
         int amountForBonus = 1_000;
-
+            blueCard:
         if (purchases >= 1 && purchases <= 15_000) {
             byte bonusesPerAmount = 50;
             int bonus = purchases / amountForBonus * bonusesPerAmount;
             int accumulatedBonuses = amountForPurchases / amountForBonus * bonusesPerAmount;
                 System.out.println(accumulatedBonuses);
                 System.out.println(bonus);
-        } if (purchases >= 15_001 && purchases <= 150_000) {
+        }
+        silverCard:
+        if (purchases >= 15_001 && purchases <= 150_000) {
             byte bonusesPerAmount = 70;
             int bonus = purchases / amountForBonus * bonusesPerAmount;
             int accumulatedBonuses = amountForPurchases / amountForBonus * bonusesPerAmount;
                 System.out.println(accumulatedBonuses);
                 System.out.println(bonus);
-        } if (purchases >= 150_000) {
+        }
+
+        if (purchases >= 150_000) {
             byte bonusesPerAmount = 100;
             int bonus = purchases / amountForBonus * bonusesPerAmount;
             int accumulatedBonuses = amountForPurchases / amountForBonus * bonusesPerAmount;
